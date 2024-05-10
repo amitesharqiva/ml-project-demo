ff86a53384ad63883558f8f176788a0d17afb8c4
amitesharqiva <amitesh.bhattacharya@arqiva.com>
2024-05-10 13:16:36 +0100

    Add to changes

---
+++ app.py
@@ -0,0 +1,9 @@
+from src.app.utils.logger import logging
+
+
+if __name__ == "__main__":
+    logging.info("Starting the application...")
+    logging.info("Application started")
+    logging.info("End of application")
+    
+    
