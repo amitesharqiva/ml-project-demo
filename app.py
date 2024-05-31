f02ff73cbc5c4ba817173a208c02fde9601507c9
amitesharqiva <amitesh.bhattacharya@arqiva.com>
2024-05-31 14:32:01 +0100

    Add Exception Changes

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from src.app.utils.logger import logging
-
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+import sys
 
 if __name__ == "__main__":
     logging.info("Starting the application...")
     logging.info("Application started")
+    
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Error occurred in the try block")
+        raise CustomException(e, sys)
+    
+    
+ 
+    
     logging.info("End of application")
     
     
